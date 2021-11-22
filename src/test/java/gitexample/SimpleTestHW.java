c3d0f085ee639b34b5ac882f8a67f7890eba666c
OlgaZtv <zatulivetrova@gmail.com>
2021-11-22 11:15:28 +0300

    added first simple test

---
+++ src/test/java/gitexample/SimpleTestHW.java
@@ -0,0 +1,12 @@
+package gitexample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTestHW {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+}
