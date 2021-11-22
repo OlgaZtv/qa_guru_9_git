826da3ca10eb31d113efdbd113fa4af713bd20ce
OlgaZtv <zatulivetrova@gmail.com>
2021-11-22 11:34:19 +0300

    added second simple test

--- src/test/java/gitexample/SimpleTestHW.java
+++ src/test/java/gitexample/SimpleTestHW.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 public class SimpleTestHW {
 
     @Test
+    void secondTest() {
+        Assertions.assertTrue(false);
+    }
+
+    @Test
     void simpleTest() {
         Assertions.assertTrue(true);
     }
