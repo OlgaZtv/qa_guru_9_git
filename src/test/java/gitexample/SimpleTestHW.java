bdbc3fc9c21654f17d5490e6a15a76776b84dee3
Olga Zatulivetrova <78685405+OlgaZtv@users.noreply.github.com>
2021-11-22 11:43:30 +0300

    added second simple test (#1)
    added second simple test

--- src/test/java/gitexample/SimpleTestHW.java
+++ src/test/java/gitexample/SimpleTestHW.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 public class SimpleTestHW {
 
     @Test
+    void secondTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
     void simpleTest() {
         Assertions.assertTrue(true);
     }
